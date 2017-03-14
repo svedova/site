a133174956c7f286256419dd1d4ca9df83cb4a3c
Leo Lamprecht <mindrun@icloud.com>
2017-03-14 14:20:12 +0100

    Support for anchor links

--- components/link.js
+++ components/link.js
@@ -1,15 +1,21 @@
 // Helpers
-import isAbsoluteUrl from 'is-absolute-url'
+import isAbsolute from 'is-absolute-url'
 
 // Components
 import Link from 'next/link'
 
+const isAnchor = href => href.charAt(0) === '#'
+
 export default ({ href, children }) => (
   <span>
     {
-      isAbsoluteUrl(href) ?
-        <a href={href} target="_blank" rel="noreferrer noopener">{children}</a> :
-        <Link href={href} prefetch><a>{children}</a></Link>
+      isAnchor(href) ?
+        <a href={href}>{children}</a> :
+        (
+          isAbsolute(href) ?
+            <a href={href} target="_blank" rel="noreferrer noopener">{children}</a> :
+            <Link href={href} prefetch><a>{children}</a></Link>
+        )
     }
 
     <style jsx>{`
