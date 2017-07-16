30ee107cde0ca1e66e810c81c8d97d45cd128801
Leo Lamprecht <mindrun@icloud.com>
2017-07-16 21:15:05 +0200

    How to prepare the renderer

--- pages/2017/electron-next.js
+++ pages/2017/electron-next.js
@@ -1,5 +1,6 @@
 // Syntax
 import bash from 'highlight.js/lib/languages/bash'
+import javascript from 'highlight.js/lib/languages/javascript'
 
 // Packages
 import Tweet from 'react-tweet-embed'
@@ -235,20 +236,29 @@ export default () =>
       As the last installation step, we need to define{' '}
       <Link href="https://github.com/leo/electron-next">electron-next</Link>,
       which ensures that Electron can handle Next.js{`'`} output in the
-      renderer:
+      renderer...
     </P>
 
     <Tweet id="881783902691577856" className="tweet" />
 
     <P>
-      It will be used both in production and development, so it needs to be
-      installed as a normal dependency:
+      ...and{' '}
+      <Link href="https://github.com/sindresorhus/electron-is-dev">
+        electron-is-dev
+      </Link>, which allows us to change the main process{`'`} behaviour
+      depending on the environment the application is running in (you{`'`}ll
+      understand why this is important later in this tutorial).
+    </P>
+
+    <P>
+      They will be used both in production and development, so they need to be
+      installed as normal dependencies:
     </P>
 
     <Code
       language="bash"
       syntax={bash}
-    >{`npm install electron-next --save`}</Code>
+    >{`npm install electron-next electron-is-dev --save`}</Code>
 
     <H3>Testing the Boilerplate</H3>
 
@@ -265,7 +275,7 @@ export default () =>
     </P>
 
     <Image
-      src="/static/essays/2017/electron-next/boilerplate.png"
+      src="/static/essays/2017/electron-next/without-next.png"
       width="512"
     />
 
@@ -273,5 +283,193 @@ export default () =>
       Saw it? Perfect, you can go on with the next section and skip the next
       sentence. If not, please repeat the steps from{' '}
       <Link href="#making-the-dream-come-true">here</Link>.
+    </P>
+
+    <H3>Preparing the New Renderer</H3>
+
+    <P>
+      Looks like we{`'`}re good to go into real detail now. Are you excited? I
+      am, at least! So let{`'`}s not waste any more time and talk about how we
+      can add our Next.js code to our Electron project.
+    </P>
+
+    <P>
+      For the module to have something to create a bundle from, we need to
+      create a directory called {`"`}renderer{`"`} (I suggest calling the
+      directory that contains the main process code {`"`}main{`"`}). Create it
+      and then also move into it:
+    </P>
+
+    <Code language="bash" syntax={bash}>{`mkdir renderer && cd renderer`}</Code>
+
+    <P>
+      Inside this directory, you can build your file and folder structure just
+      like you would do in a normal Next.js site. Take{' '}
+      <Link href="https://github.com/zeit/now-desktop/tree/be0add76aed5e27a19bbed99f0eb5490ef32a8b6/renderer">
+        Now Desktop
+      </Link>{' '}
+      as an example.
+    </P>
+
+    <P>
+      For this tutorial however, we{`'`}ll only create the most important
+      things: An entry page and the configuration file that tells Next.js how to
+      behave correctly. Let{`'`}s start with the entry page (a file named{' '}
+      <InlineCode>start.js</InlineCode> inside {`"`}pages{`"`}). For now, we{`'`}ll
+      just use basic{' '}
+      <Link href="https://facebook.github.io/react/docs/jsx-in-depth.html">
+        JSX
+      </Link>{' '}
+      to print out {`"`}Ahoy, Next.js{`"`} inside an HTML tag:
+    </P>
+
+    <Code
+      language="javascript"
+      syntax={javascript}
+    >{`export default () => (\n  <span>This is Next.js speaking</span>\n)`}</Code>
+
+    <P>
+      After you{`'`}re done, create the configuration file named{' '}
+      <InlineCode>next.config.js</InlineCode>. Inside it, you only need to do
+      handle two topics:
+    </P>
+
+    <Code language="javascript" syntax={javascript}>{`module.exports = {
+  webpack(config) {
+    // Tell webpack that you're building code
+    // for the Electron renderer. This allows
+    // it to make better decisions by itself
+    config.target = 'electron-renderer'
+
+    // Hand the config back to webpack
+    return config
+  },
+  exportPathMap() {
+    // Let Next.js know where to find the entry page
+    // when it's exporting the static bundle for the use
+    // in the production version of your app
+    return {
+      '/start': { page: '/start' }
+    }
+  }
+}`}</Code>
+
+    <P>
+      Now we{`'`}re finished with preparing a basic renderer. In the next
+      section, you{`'`}ll learn what you need to do in order for your
+      application to properly bundle the code we just added (a.k.a. how to make
+      the main process handle it).
+    </P>
+
+    <H3>Preparing the Main Process</H3>
+
+    <P>
+      In order for the renderer code to be built and reloaded automatically by
+      Next.js, we firstly need to load{' '}
+      <Link href="https://github.com/leo/electron-next">
+        electron-next
+      </Link>{' '}
+      (which we added as a dependency earlier) and call it inside the existing{' '}
+      <InlineCode>main.js</InlineCode> file (contains the main process{`'`}{' '}
+      code).
+    </P>
+
+    <P>
+      On the first line of the file, we{`'`}ll import it:
+    </P>
+
+    <Code
+      language="javascript"
+      syntax={javascript}
+    >{`const prepareNext = require('electron-next')`}</Code>
+
+    <P>
+      Then the only thing left is calling it. For this to happen, we need to
+      replace the existing event listener (which creates a window once the app
+      is ready)...
+    </P>
+
+    <Code
+      language="javascript"
+      syntax={javascript}
+    >{`app.on('ready', createWindow)`}</Code>
+
+    <P>
+      ...with one that does the same but also makes the main process handle the
+      new renderer code before creating the window:
+    </P>
+
+    <Code
+      language="javascript"
+      syntax={javascript}
+    >{`app.on('ready', async () => {
+  await prepareNext('./renderer')
+  createWindow()
+})`}</Code>
+
+    <P>
+      Now the only thing left is pointing the existing{' '}
+      <InlineCode>BrowserWindow</InlineCode> instance to the right URL depending
+      on the environment the app is running in:
+    </P>
+
+    <P>
+      In development, it should show the hot-reloading and automatically
+      re-building instance of Next.js and in production, it needs to serve the
+      static renderer files that were exported in advance (we{`'`}ll talk about
+      the exporting soon).
+    </P>
+
+    <P>
+      So please move back to the first line and load{' '}
+      <Link href="https://github.com/sindresorhus/electron-is-dev">
+        electron-is-dev
+      </Link>, a dependency we{`'`}ve added earlier in{' '}
+      <Link href="#installing-the-dependencies">this section</Link>:
+    </P>
+
+    <Code
+      language="javascript"
+      syntax={javascript}
+    >{`const isDev = require('electron-is-dev')`}</Code>
+
+    <P>
+      Next, switch to the line where the window content gets loaded using the{' '}
+      <InlineCode>.loadURL</InlineCode> method and replace it with this:
+    </P>
+
+    <Code language="javascript" syntax={javascript}>{`let entry
+
+if (isDev) {
+  entry = 'http://localhost:8000/start'
+} else {
+  entry = url.format({
+    pathname: path.resolve('./renderer/start/index.html'),
+    protocol: 'file:',
+    slashes: true
+  })
+}
+
+mainWindow.loadURL(entry)`}</Code>
+
+    <P>
+      Now, when running <InlineCode>npm start</InlineCode> to start the
+      application, you should see this:
+    </P>
+
+    <Image src="/static/essays/2017/electron-next/with-next.png" width="450" />
+
+    <P>
+      As you can see, Electron is now rendering the entry page that was built by
+      Next.js behind the curtains. When making changes to the{' '}
+      <InlineCode>start.js</InlineCode> file inside the {`"`}pages{`"`}{' '}
+      directory, you{`'`}ll notice that the code is being reloaded
+      automatically.
+    </P>
+
+    <P>
+      From here on, you can start building a full-blown new renderer using
+      Next.js inside the {`"`}renderer{`"`} directory – just like you would do
+      it when building a web app! 😏
     </P>
   </Post>
