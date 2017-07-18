5a1731c1eef796423a37880f1ec3d55ccbd3ae81
Leo Lamprecht <mindrun@icloud.com>
2017-07-18 22:36:16 +0200

    Adjusted color of links to make them more accessible

--- components/link.js
+++ components/link.js
@@ -6,24 +6,29 @@ import Link from 'next/link'
 
 const isAnchor = href => href.charAt(0) === '#'
 
-export default ({ href, children }) => (
+export default ({ href, children }) =>
   <span>
     {isAnchor(href)
-      ? <a href={href}>{children}</a>
+      ? <a href={href}>
+          {children}
+        </a>
       : isAbsolute(href)
-          ? <a href={href} target="_blank" rel="noreferrer noopener">
+        ? <a href={href} target="_blank" rel="noreferrer noopener">
+            {children}
+          </a>
+        : <Link href={href} prefetch>
+            <a>
               {children}
             </a>
-          : <Link href={href} prefetch><a>{children}</a></Link>}
+          </Link>}
 
     <style jsx>
       {`
-      a {
-        color: #4492ff;
-        text-decoration: none;
-        border-bottom: 1px dashed currentColor;
-      }
-    `}
+        a {
+          color: #0033ce;
+          text-decoration: none;
+          border-bottom: 1px dashed currentColor;
+        }
+      `}
     </style>
   </span>
-)
