c795df56bade117ca6b1678c26be5f01d84365de
Leo Lamprecht <mindrun@icloud.com>
2017-10-22 15:57:54 +0200

    Perfected style for post

--- pages/2017/zeit-day-april.js
+++ pages/2017/zeit-day-april.js
@@ -7,6 +7,7 @@ export default asPost({
   id: 'zeit-day-april',
   hasCover: true
 })(markdown(components)`
+
   When we had our first internal discussion about potentially
   making a conference happen around [ZEIT](https://zeit.co),
   its products and [its community](https://zeit.chat), I got very
@@ -296,4 +297,5 @@ export default asPost({
   I'll just leave this one thing here: There will
   be another [ZEIT Day](https://zeit.co/day) soon - and I'll
   be there, no doubt! 😜
+
 `)
